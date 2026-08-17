f06f84eb483fd7462d05070fc23609ed721f55b7
Pancosta <dz6.gustav@gmail.com>
2026-08-17 18:31:55 -0300

    chore: initial project setup with axum REST API for asset management

---
+++ src/app.rs
@@ -0,0 +1,84 @@
+use std::sync::Arc;
+
+use axum::{Json, Router, extract::State, routing::get};
+use serde::Deserialize;
+use tokio::{net::TcpListener, sync::Mutex};
+use tracing::info;
+use tracing_subscriber::{
+    Layer, fmt::format::FmtSpan, layer::SubscriberExt, util::SubscriberInitExt,
+};
+
+use crate::models::Asset;
+
+#[derive(Clone)]
+pub struct AppState {
+    pub assets: Arc<Mutex<Vec<Asset>>>,
+}
+
+impl AppState {
+    fn new() -> Self {
+        Self {
+            assets: Default::default(),
+        }
+    }
+}
+
+pub struct App;
+
+impl App {
+    pub async fn start() -> color_eyre::Result<()> {
+        let layer = tracing_subscriber::fmt::layer()
+            .with_span_events(FmtSpan::NEW)
+            .boxed();
+
+        tracing_subscriber::registry().with(layer).init();
+
+        let listener = TcpListener::bind("0.0.0.0:3000").await?;
+
+        let router = Router::new()
+            .route("/", get(Self::list_assets).post(create_asset))
+            .with_state(AppState::new());
+
+        info!("Starting service");
+
+        axum::serve(listener, router).await?;
+
+        Ok(())
+    }
+
+    #[tracing::instrument(skip_all)]
+    async fn list_assets(state: State<AppState>) -> Json<Vec<Asset>> {
+        let assets = state.assets.lock().await;
+        Json(assets.clone())
+    }
+}
+
+#[derive(Deserialize)]
+struct CreateAssetsRequest {
+    name: String,
+    unit_value: f64,
+}
+
+#[tracing::instrument(skip_all)]
+async fn create_asset(
+    state: State<AppState>,
+    Json(request): Json<CreateAssetsRequest>,
+) -> Json<Asset> {
+    let mut assets = state.assets.lock().await;
+
+    let id = assets
+        .iter()
+        .map(|asset| asset.id)
+        .max()
+        .unwrap_or_default()
+        + 1;
+
+    let new_asset = Asset {
+        id,
+        name: request.name,
+        unit_value: request.unit_value,
+    };
+    assets.push(new_asset.clone());
+
+    Json(new_asset)
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+mod app;
+pub mod models;
+use crate::app::App;
+
+#[tokio::main]
+async fn main() -> color_eyre::Result<()> {
+    App::start().await
+}
